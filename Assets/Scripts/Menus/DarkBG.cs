bb2b22c26ec2f8c88fa7f7e0006a062d343ae202
RobertG-H <robertgrahamhu@gmail.com>
2019-09-29 20:06:30 +0900

    Added basic juice like music, sfx, and menus. - Added yeti splash screen - Added title screen - Added music to all screens - Added sfx to player - Added Camera shake to player on damage

---
+++ Assets/Scripts/Menus/DarkBG.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkBG : MonoBehaviour {
+    Animator anim;
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    // Called by start game button
+    public void StartGame() {
+        anim.SetTrigger("fadeOut");
+    }
+}
